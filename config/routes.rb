07aaee259b1ebb372d83e50544d3dd794939c53a
Jaida <jaidag03@gmail.com>
2018-08-19 21:53:27 +0000

    demo the site to make sure it was working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
     get 'todo/show', to: 'todo#show'
+    get 'todo/create', to: 'todo#create'
+    get 'todo/new', to: 'todo#new'
+    get 'todo/edit/:id', to: 'todo#edit'
+    get 'todo/update/:id', to: 'todo#update'
+    get 'todo/destroy/:id', to: 'todo#destroy'
+    root to: 'todo#index'
 get 'todo/index', to: 'todo#index'
 end
